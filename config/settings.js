e5a949022cb2af6ef2369b4c3d4ced32b9e155cd
roman <rsng@mail.ru>
2016-07-06 11:18:55 +0300

    Es monthly index.

--- config/settings.js
+++ config/settings.js
@@ -1,20 +1,37 @@
+var moment = require('moment');
+var _ = require('lodash');
 var auth = 'es:Elastic777';
 
+const MONTHS = 6;
+
+var calculateIndexName = function (months) {
+    var now = moment.utc();
+    var parts = [];
+    parts.push({year: now.year(), month: now.month()});
+
+    _.times(months - 1, function () {
+        now.subtract(1, 'months');
+        parts.push({year: now.year(), month: now.month()});
+    });
+
+    return parts.map((x)=>'wobot-monthly-y' + x.year + '*m' + (x.month + 1)).join(',');
+};
+
+
 module.exports = {
-    es:{
-        // hosts: ['127.0.0.1:9200'],
+    es: {
         hosts: [
             {host: '91.218.113.136', port: 9200, auth: auth},
             {host: '91.210.105.210', port: 9200, auth: auth}
         ],
-        // log: 'trace',
-        index: 'wobot33'
+        get index() {
+            return calculateIndexName(MONTHS);
+        }
     },
-    mongo:{
-        // url: 'mongodb://localhost:27017/focus_crawl_db',
-        url: 'mongodb://91.218.113.211:27035/focus_crawl_db'
+    mongo: {
+        url: 'mongodb://91.218.113.211:27018/focus_crawl_db'
     },
-    app:{
-        port:3001
+    app: {
+        port: 3001
     }
 };
